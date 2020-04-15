d9cf73d449b34b2316f18e62d22729aa02ccb6fe
Git-lei <18332708701@163.com>
2020-04-15 09:17:25 +0800

    德州 电脑端 响应式

---
+++ js/web.js
@@ -0,0 +1,54 @@
+$(function(){
+	
+    "use strict";
+    /*寮圭獥*/
+    $(".queren").click(function(){
+        
+        $(".tc_out").fadeIn();
+        
+    $(".xx").click(function(){
+        
+        $(".tc_out").fadeOut();
+    });		
+    });	
+        
+     
+    $(".btn").click(function(){
+        $(".navv").slideToggle();	
+        
+    });
+    
+    
+    for(var i=0; i<100; i++){
+        
+        if(i%3===0){
+            $(".compa li").eq(i).css({"margin-left":"0"});	
+        }
+        
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
+    });
